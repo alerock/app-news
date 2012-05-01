98afad7acc0c275f018a3e4b241fb3c228492e20
Alexandre Rocha <alerock@gmail.com>
2012-05-01 12:21:44 -0400

    alteracao de teste

--- src/main/java/br/fucapi/workshop/app_news/GeradorNoticia.java
+++ src/main/java/br/fucapi/workshop/app_news/GeradorNoticia.java
@@ -10,7 +10,7 @@ public class GeradorNoticia {
 		List<Noticia> noticias = new ArrayList<Noticia>();
 		
 		Noticia noticia1 = new Noticia("Titulo 1", "http://www.ig.com.br");
-		Noticia noticia2 = new Noticia("Titulo 2", "http://www.uol.com.br");
+		Noticia noticia2 = new Noticia("Titulo 4", "http://www.uol.com.br");
 		
 		noticias.add(noticia1);
 		noticias.add(noticia2);
